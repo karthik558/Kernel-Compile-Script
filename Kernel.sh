940bf88e2c7e3fe1a6f19bff3d6366d1d7bbec97
Karthik SP <53151501+karthik558@users.noreply.github.com>
2020-07-15 02:24:14 +0530

    Add files via upload
    Flasher - Disabled ld.lld
    Kernel.sh - CLANG Script
    run.sh - merged Fasher.sh and Kernel.sh 
    rungcc.sh - GCC Script

---
+++ Kernel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export ARCH=arm64
+export SUBARCH=arm64
+TC_DIR="/home/karthiksp/Kernel"
+MPATH="$TC_DIR/CLANG/bin/:$PATH"
+rm -f out/arch/arm64/boot/Image.gz-dtb
+make O=out vendor/violet-perf_defconfig
+PATH="$MPATH" make -j4 O=out \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    LD=ld.lld \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+        CC=clang \
+        AR=llvm-ar \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip
+        2>&1 | tee error.log
+
+cp out/arch/arm64/boot/Image.gz-dtb /home/karthiksp/Kernel/Anykernel
+cd /home/karthiksp/Kernel/Anykernel
+if [ -f "Image.gz-dtb" ]; then
+    zip -r9 Ryzen+-violet-"$DATE".zip * -x .git README.md *placeholder
+    echo "Build success!"
+else
+    echo "Build failed!"
+fi
