4baefe125623a09c9f13210724880793789c1705
Karthik SP <53151501+karthik558@users.noreply.github.com>
2020-09-10 13:15:53 +0530

    Update run.sh
    Updated cpu cores , changed path , and import ziping commands

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-<<<< Basic Script to build kernel >>>>
+#Basic Script to build kernel 
 
 #!/bin/bash
 export ARCH=arm64
 export SUBARCH=arm64
-TC_DIR="/home/"username"/Kernel"
-MPATH="$TC_DIR/CLANG/bin/:$PATH"
+TC_DIR="/home/ubuntu/Kernel"
+MPATH="$TC_DIR/clang12/bin/:$PATH"
 rm -f out/arch/arm64/boot/Image.gz-dtb
 make O=out vendor/violet-perf_defconfig
-PATH="$MPATH" make -j4 O=out \
+PATH="$MPATH" make -j32 O=out \
     NM=llvm-nm \
     OBJCOPY=llvm-objcopy \
     LD=ld.lld \
@@ -18,3 +18,12 @@ PATH="$MPATH" make -j4 O=out \
         OBJDUMP=llvm-objdump \
         STRIP=llvm-strip
         2>&1 | tee error.log
+
+cp out/arch/arm64/boot/Image.gz-dtb /home/ubuntu/Kernel/Anykernel
+cd /home/ubuntu/Kernel/Anykernel
+if [ -f "Image.gz-dtb" ]; then
+    zip -r9 Ryzen+-violet-"$DATE".zip * -x .git README.md *placeholder
+    echo "Build success!"
+else
+    echo "Build failed!"
+fi
