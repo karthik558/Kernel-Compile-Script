af4cdee560eeeab4f4877e17250999334833531e
Karthik SP <53151501+karthik558@users.noreply.github.com>
2020-09-24 16:50:14 +0530

    Included support for pushing zip direct to GDRIVE

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 export ARCH=arm64
 export SUBARCH=arm64
 TC_DIR="/home/ubuntu/Kernel"
-MPATH="$TC_DIR/clang12/bin/:$PATH"
+MPATH="$TC_DIR/clang/bin/:$PATH"
 rm -f out/arch/arm64/boot/Image.gz-dtb
 make O=out vendor/violet-perf_defconfig
 PATH="$MPATH" make -j32 O=out \
@@ -20,9 +20,13 @@ PATH="$MPATH" make -j32 O=out \
         2>&1 | tee error.log
 
 cp out/arch/arm64/boot/Image.gz-dtb /home/ubuntu/Kernel/Anykernel
-cd /home/ubuntu/Kernel/Anykernel
+cd /home/ubuntu/Kernel/Anykernel 
 if [ -f "Image.gz-dtb" ]; then
     zip -r9 Ryzen+-violet-"$DATE".zip * -x .git README.md *placeholder
+cp /home/ubuntu/Kernel/Anykernel/Ryzen+-violet-"$DATE".zip /home/ubuntu/Kernel
+./gdrive list
+./upload yzen+-violet-"$DATE".zip
+
     echo "Build success!"
 else
     echo "Build failed!"
