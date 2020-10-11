476c5dfc32ecc1108a929059b149e37b2a2e690c
Karthik SP <53151501+karthik558@users.noreply.github.com>
2020-10-12 00:51:49 +0530

    Fix script

--- run.sh
+++ run.sh
@@ -19,11 +19,14 @@ PATH="$MPATH" make -j32 O=out \
         STRIP=llvm-strip
         2>&1 | tee error.log
 
+git clone https://android.googlesource.com/platform/system/libufdt scripts/ufdt/libufdt
+python2 scripts/ufdt/libufdt/utils/src/mkdtboimg.py create out/arch/arm64/boot/dtbo.img --page_size=4096 out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo
 cp out/arch/arm64/boot/Image.gz-dtb /home/ubuntu/Kernel/Anykernel
+cp out/arch/arm64/boot/dtbo.img /home/ubuntu/Kernel/Anykernel
 cd /home/ubuntu/Kernel/Anykernel 
 if [ -f "Image.gz-dtb" ]; then
-    zip -r9 Ryzen+-violet-"$DATE".zip * -x .git README.md *placeholder
-cp /home/ubuntu/Kernel/Anykernel/Ryzen+-violet-"$DATE".zip /home/ubuntu/Kernel
+    zip -r9 Ryzen+-violet-dtbo-$(TZ=Asia/Kolkata date +'%M%H-%d%m%Y').zip"* -x .git README.md *placeholder
+cp /home/ubuntu/Kernel/Anykernel/Ryzen+-violet-dtbo-$(TZ=Asia/Kolkata date +'%M%H-%d%m%Y').zip /home/ubuntu/Kernel
 
 #Clonning GDrive to Main DIR
 wget https://www.dropbox.com/s/w65lffvkkqvvj93/gdrive?dl=1
@@ -32,8 +35,10 @@ chmod +x gdrive
 ./gdrive list
 ./gdrive upload filename
 ./gdrive list 
-./gdrive upload Ryzen+-violet-"$DATE".zip
+./gdrive upload Ryzen+-violet-dtbo-$(TZ=Asia/Kolkata date +'%M%H-%d%m%Y').zip
 
+elapsed=$((build_complete-build_start))
+echo "Completed Successfully in $elapsed Seconds."
     echo "Build success!"
 else
     echo "Build failed!"
