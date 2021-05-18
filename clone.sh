e1674c4388a50adfe9cd0b29da0323e3440a5e3e
karthik558 <karthik.lal558@gmail.com>
2021-05-18 15:31:52 +0530

    script: import clone script and update current script
    * hehe

---
+++ clone.sh
@@ -0,0 +1,9 @@
+# Kernel Clonning Script
+
+git clone https://github.com/karthik558/MsM-4.14-RyZeN- RyZeN
+git clone https://github.com/karthik558/AnyKernel3 Anykernel
+git clone https://github.com/kdrag0n/proton-clang CLANG-13
+
+# Start Build
+
+bash run.sh
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#Kernel-Compiling-Script 
+#Kernel-Compiling-Script
 
 #!/bin/bash
 export ARCH=arm64
@@ -14,11 +14,14 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
+# Set Date
+DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
+
 TC_DIR="/home/ubuntu/Kernel"
 MPATH="$TC_DIR/clang/bin/:$PATH"
 rm -f out/arch/arm64/boot/Image.gz-dtb
 make O=out vendor/violet-perf_defconfig
-PATH="$MPATH" make -j32 O=out \
+PATH="$MPATH" make -j16 O=out \
     NM=llvm-nm \
     OBJCOPY=llvm-objcopy \
     LD=ld.lld \
@@ -32,14 +35,15 @@ PATH="$MPATH" make -j32 O=out \
 
 git clone https://android.googlesource.com/platform/system/libufdt scripts/ufdt/libufdt
 python2 scripts/ufdt/libufdt/utils/src/mkdtboimg.py create out/arch/arm64/boot/dtbo.img --page_size=4096 out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo
+
 cp out/arch/arm64/boot/Image.gz-dtb /home/ubuntu/Kernel/Anykernel
 cp out/arch/arm64/boot/dtbo.img /home/ubuntu/Kernel/Anykernel
-cd /home/ubuntu/Kernel/Anykernel 
+cd /home/ubuntu/Kernel/Anykernel
 if [ -f "Image.gz-dtb" ]; then
-    zip -r9 Ryzen+-violet-dtbo-$(TZ=Asia/Kolkata date +'%M%H-%d%m%Y').zip"* -x .git README.md *placeholder
-cp /home/ubuntu/Kernel/Anykernel/Ryzen+-violet-dtbo-$(TZ=Asia/Kolkata date +'%M%H-%d%m%Y').zip /home/ubuntu/Kernel
+    zip -r9 RyZeN+-violet-R-"$DATE".zip"* -x .git README.md *placeholder
+cp /home/ubuntu/Kernel/Anykernel/RyZeN+-violet-R-"$DATE".zip /home/ubuntu/Kernel
 rm /home/ubuntu/Kernel/Anykernel/Image.gz-dtb
-rm /home/ubuntu/Kernel/Anykernel/Ryzen+-violet-dtbo-$(TZ=Asia/Kolkata date +'%M%H-%d%m%Y').zip
+rm /home/ubuntu/Kernel/Anykernel/RyZeN+-violet-R-"$DATE".zip
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
