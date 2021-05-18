673d77d90808277649b4298c4920537a79ea40ca
karthik558 <karthik.lal558@gmail.com>
2021-05-18 15:41:20 +0530

    script: Damh forgot to add depth tag

--- clone.sh
+++ clone.sh
@@ -1,8 +1,8 @@
 # Kernel Clonning Script
 
 git clone https://github.com/karthik558/MsM-4.14-RyZeN- RyZeN
-git clone https://github.com/karthik558/AnyKernel3 Anykernel
-git clone https://github.com/kdrag0n/proton-clang CLANG-13
+git clone --depth=1 https://github.com/karthik558/AnyKernel3 Anykernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang CLANG-13
 
 # Start Build
 
